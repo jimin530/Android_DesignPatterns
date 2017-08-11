f69479e104b5473447c5109d179a065fab9723ef
Jimin Kim <jimin6407@naver.com>
2017-08-12 00:35:58 +0900

    Init MVC

---
+++ BoxOfficeChart/app/src/main/java/com/jmdroid/boxofficechart/mvc/MvcMainActivity.java
@@ -0,0 +1,15 @@
+package com.jmdroid.boxofficechart.mvc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jmdroid.boxofficechart.R;
+
+public class MvcMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvc_main);
+    }
+}
